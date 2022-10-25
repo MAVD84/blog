b8100936b47c1a73b810486c8e2ae528bc6a8d0b
Miguel Angel Vega Duran <91657534+MAVD84@users.noreply.github.com>
2022-10-25 17:16:13 -0500

    Update Email Verification subscription_handler.php

--- Email Verification subscription_handler.php
+++ Email Verification subscription_handler.php
@@ -1,3 +1,31 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<title>Email Verification with PHP by CodexWorld</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- web-fonts -->
+<link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+
+<!-- Stylesheet file -->
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body class="subs">
+<div class="container">
+    <div class="subscribe box-sizing">
+        <div class="sloc-wrap box-sizing">
+            <div class="sloc-content">
+                <div class="sloc-text">
+                    <div class="sloc-header"><?php echo $statusMsg; ?></div>
+                </div>
+                <a href="<?php echo $siteURL; ?>" class="cwlink">Go to Site</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
 // Include config file 
 require_once 'config.php'; 
  
@@ -31,31 +59,6 @@ if(!empty($_GET['email_verify'])){
         $statusMsg = '<p class="error">You have clicked on the wrong link, please check your email and try again.</p>'; 
     } 
 ?> 
- 
-
-<!DOCTYPE html>
-<html lang="en-US">
-<head>
-<title>Email Verification with PHP by CodexWorld</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<!-- web-fonts -->
-<link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
-
-<!-- Stylesheet file -->
-<link rel="stylesheet" type="text/css" href="css/style.css" />
-</head>
-<body class="subs">
-<div class="container">
-    <div class="subscribe box-sizing">
-        <div class="sloc-wrap box-sizing">
-            <div class="sloc-content">
-                <div class="sloc-text">
-                    <div class="sloc-header"><?php echo $statusMsg; ?></div>
-                </div>
-                <a href="<?php echo $siteURL; ?>" class="cwlink">Go to Site</a>
-            </div>
-        </div>
-    </div>
-</div>
+</script>
+</body>
+</html>
